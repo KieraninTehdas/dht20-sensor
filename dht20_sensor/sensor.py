2d60fcfbb28378362bf72941bbfa602ce3c78acf
Kieran Porter <kieranintehdas@gmail.com>
2022-09-10 00:49:21 +0100

    Working class for getting timestamped temperature and humidity readings from the DHT20 sensor

--- dht20_sensor/sensor.py
+++ dht20_sensor/sensor.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+from typing import Tuple
 
 from smbus2 import SMBus
 
@@ -18,17 +19,17 @@ class TemperatureReading:
     def __init__(self, value: float, timestamp: datetime):
         self.value = value
         self.timestamp = timestamp
-        self.units = "degrees centigrade"
+        self.units = "C"
 
     def __str__(self) -> str:
-        return f"{self.value} {self.units}"
+        return f"{self.value}{self.units}"
 
+class InitialisationError(Exception):
+    pass
 
 class DHT20Sensor:
     def __init__(self, address=0x38):
         self.address = address
-        self.latest_temperature = None
-        self.latest_humidity = None
 
         # Wait for sensor to stabilise and reach idle state
         time.sleep(0.2)
@@ -36,33 +37,30 @@ class DHT20Sensor:
         with SMBus(1) as bus:
             status_byte = bus.read_i2c_block_data(self.address, 0x71, 1)
 
-        if status_byte[0] != 0x18:
-            print("Initialisation Error")
+        if status_byte[0] & 0x18 != 0x18:
+            raise InitialisationError("Sensor is not initialised")
 
         time.sleep(0.2)
-
-    def read(self):
+    
+    def read(self) -> Tuple[TemperatureReading, RelativeHumidityReading]:
         with SMBus(1) as bus:
             bus.write_i2c_block_data(self.address, 0xAC, [0x33, 0x00])
             time.sleep(0.1)
             data = bus.read_i2c_block_data(self.address, 0x71, 7)
 
-        reading_time = datetime.utcnow()
+        reading_time = datetime.utcnow().timestamp()
         temperature = TemperatureReading(self._extract_temperature(data), reading_time)
         humidity = RelativeHumidityReading(self._extract_humidity(data), reading_time)
-
-        self.latest_temperature = temperature
-        self.latest_humidity = humidity
 
         return (temperature, humidity)
 
 
-    def _extract_temperature(self, raw_data) -> float:
+    def _extract_temperature(self, data: list) -> float:
         raw_temperature = ((data[3] & 0xF) << 16) + (data[4] << 8) + data[5]
 
         return 200 * (float(raw_temperature) / (2**20)) - 50
 
-    def _extract_humidity(self, raw_data) -> float:
+    def _extract_humidity(self, data: list) -> float:
         raw_humidity = (data[1] << 12) + (data[2] << 4) + ((data[3] & 0xf0) >> 4)
 
         return 100 * (float(raw_humidity) / (2**20))
